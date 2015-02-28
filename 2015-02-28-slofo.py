b97586b4875fbd12b5b61178d9cba6f6fdf84867
Erik Schoster <erik@hecanjog.com>
2015-02-28 14:06:00 -0600

    Bugfixes to slofo

--- 2015-02-28-slofo.py
+++ 2015-02-28-slofo.py
@@ -7,22 +7,25 @@ length = dsp.stf(120)
 
 layers = []
 for freq in chord:
-    lfos = []
-    for lfo in range(6):
-        lf = dsp.rand(0.1, 1)
-        lwf = dsp.breakpoint([ dsp.rand(0, 1) for p in range(5) ], 512)
-        lfo = dsp.ctone(lf, length, lwf, dsp.rand(0.75, 1))
-        lfos += [ lfo ]
+    takes = []
+    for take in range(4):
+        lfos = []
+        for lfo in range(3):
+            lf = dsp.rand(0.01, 0.1)
+            lwf = dsp.breakpoint([0] + [ dsp.rand(0, 1) for p in range(5) ] + [0], 512)
+            lfo = dsp.ctone(lf, length, lwf, dsp.rand(0.75, 1))
+            lfos += [ lfo ]
 
-    lfo = dsp.mix(lfos)
+        wf = dsp.wavetable('tri', 512)
+        amp = 0.5
 
-    wf = dsp.wavetable('tri', 512)
-    amp = 0.2
+        take = dsp.ctone(freq, length, wf, amp)
+        for lfo in lfos:
+            take = dsp.am(take, lfo)
 
-    layer = dsp.ctone(freq, length, wf, amp)
-    layer = dsp.mul(layer, lfo)
+        takes += [ dsp.pan(take, dsp.rand()) ]
 
-    layers += [ layer ]
+    layers += [ dsp.mix(takes) ]
 
 out = dsp.mix(layers)
 
