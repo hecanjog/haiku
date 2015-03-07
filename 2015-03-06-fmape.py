56c6fe50e71596c0e7dd5767bdb5670133deb267
Erik Schoster <erik@hecanjog.com>
2015-03-06 18:24:24 -0600

    Updated fmape

--- 2015-03-06-fmape.py
+++ 2015-03-06-fmape.py
@@ -9,7 +9,6 @@ api_key = os.environ.get('FMA_API_KEY')
 endpoint = 'http://freemusicarchive.org/api/get/tracks.json'
 maxtrack_id = 116020 # thanks ross!
 
-numtracks = 10
 numsections = 10
 
 out = ''
@@ -17,16 +16,23 @@ out = ''
 for section in range(numsections):
     tracks = []
     foundtracks = 0
+    numtracks = dsp.randint(3, 10) 
+
     while foundtracks <= numtracks:
         track_id = dsp.randint(1, maxtrack_id)
         params = {'api_key': api_key, 'track_id': track_id}
         track = requests.get(endpoint, params=params)
         try:
             track = track.json()
+            track_license = track['dataset'][0]['license_title']
 
-            if len(track['errors']) == 0:
+            if len(track['errors']) == 0 and not 'Deriv' in track_license:
 
                 track_url = track['dataset'][0]['track_url']
+                track_name = track['dataset'][0]['track_title']
+                track_artist = track['dataset'][0]['artist_name']
+
+                dsp.log('%s by %s. %s' % (track_name, track_artist, track_license))
 
                 track_page = requests.get(track_url)
                 html = BeautifulSoup(track_page.text)
@@ -44,7 +50,7 @@ for section in range(numsections):
 
                 snd = dsp.read('%s.wav' % track_id).data
 
-                tracks += [ snd ]
+                tracks += [ dsp.amp(snd, 0.25) ]
                 
                 os.remove('%s.mp3' % track_id)
                 os.remove('%s.wav' % track_id)
@@ -54,7 +60,6 @@ for section in range(numsections):
         except Exception:
             pass
     
-    for track in tracks:
-        out += fx.spider(track)
+    out += dsp.mix([ fx.spider(track, dsp.randint(5, 20), dsp.randint(10, 50), dsp.rand(30, 50), (dsp.rand(5, 50), dsp.rand(200, 1500)), dsp.randchoose([True, False])) for track in tracks ])
 
 dsp.write(out, 'fmape')
